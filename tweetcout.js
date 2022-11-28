05fd4f8b3a2170d26bb44c57a9bfdb5be08b9866
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-28 18:15:24 +0530

    Create tweetcout.js

---
+++ tweetcout.js
@@ -0,0 +1,5 @@
+var tweet=prompt("enter your tweet :");
+var leng=tweet.length;
+var totallen=180;
+var remain=totallen-leng;
+alert("you have written " + leng + " characters" + " remaining characters is " + remain);
